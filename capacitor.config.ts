b6feb2b29741d60aef7832388097980edbc73791
Darleanow <hugonnierenzo@gmail.com>
2023-04-20 10:32:28 +0200

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.darleanow.lostPets',
+  appName: 'lost-pets',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
